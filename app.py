73836d5ef5c2b839ebe5cfed4e94bd5f00d0985c
harsha8104-arjun <hvardhan.df1@gmail.com>
2026-01-01 16:35:45 +0530

    Fix indentation for model.generate output

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ Output code only.
 
             inputs = tokenizer(full_prompt, return_tensors="pt")
 
-           outputs = model.generate(**inputs, max_new_tokens=200,temperature=0.0,do_sample=False,eos_token_id=tokenizer.eos_token_id)
+            outputs = model.generate(**inputs, max_new_tokens=200,temperature=0.0,do_sample=False,eos_token_id=tokenizer.eos_token_id)
 
             raw = tokenizer.decode(output[0], skip_special_tokens=True)
 
